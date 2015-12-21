f35b1495bfba03710053bc9fea116891d3cc48d3
Edwin Choi <edwinc91@Edwin-Chois-MBP.home>
2015-12-21 08:22:05 -0500

    added some more changes

---
+++ public/js/app.js
@@ -0,0 +1,35 @@
+var app = angular.module('TodoApp', []);
+
+app.controller('CreateController', ['$http', '$scope', function($http, $scope){
+  var controller = this;
+  $http.get('items').success(function(data){
+    controller.todos = data;
+  });
+
+  this.create = function(){
+    $http.post('/items', {
+      todo_value: this.value
+    }).success(function(data){
+      console.log(data);
+      controller.todos = data;
+    });
+  };
+
+  this.delete = function(id){
+    $http.delete('/items/' + id).success(function(data){
+      console.log(data);
+      controller.todos = data;
+    });
+  };
+  // this.patch = function() {
+  //   $http.patch('/items' + item.id, {
+  //     todo_value: this.value
+  //   }).success(function(data){
+  //     console.log(data);
+  //     controller.todos = data;
+  //   });
+  // };
+  // this.delete = function() {
+  //   $http.delete('/items')
+  // }
+}]);
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express         = require('express'),
     // PORT            = process.env.PORT || 3001,
     server          = express(),
     // MONGOURI        = process.env.MONGOLAB_URI || "mongodb://localhost:27017",
-    dbname          = "feddit",
+    dbname          = "tododb",
     mongoose        = require('mongoose'),
     Schema          = mongoose.Schema,
     ejs             = require('ejs'),
@@ -10,3 +10,59 @@ var express         = require('express'),
     bodyParser      = require('body-parser'),
     methodOverride  = require('method-override'),
     session         = require('express-session');
+
+var itemSchema = new Schema({
+    todo_value: { type: String, required: true },
+    created: { type: Date, default: Date.now }
+  });
+var Item = mongoose.model('Item', itemSchema);
+
+server.set('views', './views');
+server.set('view engine', 'ejs');
+
+server.use(session({
+  secret: "mysecret",
+  resave: true,
+  saveUninitialized: false
+}));
+
+server.use(express.static('./public'));
+server.use(bodyParser.json());
+server.use(expressLayouts);
+server.use(methodOverride('_method'));
+
+  // server.get('/items', function(request, response){
+  //   mongo.collection('items').find({}).toArray(function(err, data){
+  //     response.json(data);
+  //   });
+  // });
+
+server.get('/items', function(request, response){
+  Item.find({}, function(err, items){
+    response.json(items);
+  })
+});
+
+
+server.post('/items', function(request, response){
+  Item.create(request.body, function(err, data){
+    Item.find({}, function(err, items){
+      response.json(items);
+    });
+  });
+});
+
+server.delete('/items/:item_id', function(request, response){
+  Item.remove({
+    _id : request.params.item_id
+  }, function(err, data){
+    Item.find({}, function(err, items){
+      response.json(items);
+    });
+  });
+});
+
+mongoose.connect('mongodb://localhost:27017/tododb');
+server.listen(3000, function(){
+  console.log("Server is listening");
+});
